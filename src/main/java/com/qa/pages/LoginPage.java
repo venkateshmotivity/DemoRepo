d41b2b0fa590f2e28635bc80fba4da69aea237e7
venkateshmotivity <venkateswara.g@motivitylabs.com>
2019-09-25 11:55:54 +0530

    First code into ggit

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class LoginPage {
+	 public void loginpage()
+	 {
+		 System.out.println("Login Page");
+	 }
+
+}
---
+++ src/main/java/com/qa/utilites/TestUtil.java
@@ -0,0 +1,9 @@
+package com.qa.utilites;
+
+public class TestUtil {
+
+	public void comm()
+	{
+		System.out.println("Test Utilities");
+	}
+}
